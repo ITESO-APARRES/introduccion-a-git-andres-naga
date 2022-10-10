fad70232742d7dce7cc02c1d4073616d20a424a1
andres-naga <115427590+andres-naga@users.noreply.github.com>
2022-10-10 01:30:27 -0500

    paso 3 commit 3 hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
+/* Archivo Hello.c que usamos para el ejercicio de manejo de git basico*/
+    
+
 #include <stdio.h>
 
 int main(void){
     printf("Hola mundo");
-    print("\nHello word");
     return 0;
 }
